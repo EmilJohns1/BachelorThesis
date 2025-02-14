81439907e867436869d518093b6ded2d6407907d
Zahid André Kristiansen <zahidak@stud.ntnu.no>
2025-02-14 14:05:15 +0100

    Need to change imports and integration with util

--- dqnlearningagent.py
+++ dqnlearningagent.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from util import plot_rewards, write_to_json
 from env_manager import EnvironmentManager
 
 
--- qlearning_agent.py
+++ qlearning_agent.py
@@ -5,5 +5,4 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from util import plot_rewards, write_to_json
 from env_manager import EnvironmentManager
