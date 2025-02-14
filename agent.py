c3b4c17c730ec4370a4854ed0914396cca4fa83a
Zahid André Kristiansen <zahidak@stud.ntnu.no>
2025-02-14 14:20:43 +0100

    used the dev versions of main and agent

--- agent.py
+++ agent.py
@@ -1,42 +1,54 @@
 import numpy as np
 
+
 class Agent:
-    def __init__(self, model, gaussian_width=0.3, exploration_rate=0.1, use_clusters=False):
+    def __init__(
+        self, model, gaussian_width=0.3, exploration_rate=0.1, use_clusters=False
+    ):
         self.model = model
         self.gaussian_width = gaussian_width
         self.exploration_rate = exploration_rate
         self.use_clusters = use_clusters
 
     def normalize_states(self):
-        states_mean = np.array([0., 0., 0., 0.])
-        states_std = np.array([1., 1., 1., 1.])
+        states_mean = np.array([0.0, 0.0, 0.0, 0.0])
+        states_std = np.array([1.0, 1.0, 1.0, 1.0])
 
         if len(self.model.states) > 0:
             states_mean = self.model.states_mean
             states_std = self.model.states_std
 
         for i, _ in enumerate(states_std):
-            if states_std[i] == 0.:
-                states_std[i] = 1.
+            if states_std[i] == 0.0:
+                states_std[i] = 1.0
         return states_mean, states_std
 
-    def compute_action_rewards(self, state, states_mean, states_std):      
-        action_rewards = [0. for _ in self.model.actions]
-        action_weights = [0. for _ in self.model.actions]
-        
+    def compute_action_rewards(self, state, states_mean, states_std):
+        action_rewards = [0.0 for _ in self.model.actions]
+        action_weights = [0.0 for _ in self.model.actions]
+
         # Ensure transitions refer to valid cluster indices
         for action in self.model.actions:
             if len(self.model.state_action_transitions_from[action]) > 0:
                 dist = (state - states_mean) / states_std - (
-                    self.model.states[self.model.state_action_transitions_from[action]] - states_mean
+                    self.model.states[self.model.state_action_transitions_from[action]]
+                    - states_mean
                 ) / states_std
                 weight = np.exp(-np.sum(np.square(dist), axis=1) / self.gaussian_width)
                 action_weights[action] = np.sum(weight)
-                action_rewards[action] = np.sum(weight * self.model.rewards[
-                    np.array(self.model.state_action_transitions_to[action], dtype=int)  # Ensure it's an integer array
-                ]) / action_weights[action]
+                action_rewards[action] = (
+                    np.sum(
+                        weight
+                        * self.model.rewards[
+                            np.array(
+                                self.model.state_action_transitions_to[action],
+                                dtype=int,
+                            )  # Ensure it's an integer array
+                        ]
+                    )
+                    / action_weights[action]
+                )
         return action_rewards, action_weights
-
 
     def get_action(self, action_rewards, action_weights):
         for action in self.model.actions:
@@ -45,5 +57,7 @@ class Agent:
             if action_weights[action] == 0:
                 return action  # Return action that has never been chosen before
             if action_weights[action] / np.max(action_weights) < self.exploration_rate:
-                return action  # Return action that has little data for the current state
+                return (
+                    action  # Return action that has little data for the current state
+                )
         return np.argmax(action_rewards)
--- main.py
+++ main.py
@@ -1,101 +1,115 @@
+import random
+import time
+import pygame
+from agent import Agent
 from env_manager import EnvironmentManager
 from model import Model
-from agent import Agent
-import pygame
+from util.cluster_visualizer import ClusterVisualizer
+from util.logger import write_to_json
+from util.reward_visualizer import plot_rewards
+
 import numpy as np
-import time
-from util import plot_rewards, write_to_json
 
-#################################################
-# These variables should be logged for each run
-environment = "CartPole-v1"
-discount_factor = 1
-training_time = 160
-testing_time = 40
-training_rewards = []
-testing_rewards = []
-k = 4000
-#################################################
+for i in range(20):
+    #################################################
+    # These variables should be logged for each run
+    environment = "CartPole-v1"
+    discount_factor = 1
+    k = 3000
+    gaussian_width_rewards = 0.5
+    seed = random.randint(0, 2 ** 32 - 1)
+    comments = ""
+    training_time = 100
+    testing_time = 100
+    training_rewards = []
+    testing_rewards = []
+    #################################################
 
-episode_rewards = []
-render_mode = None  # Set to None to run without graphics
+    episode_rewards = []
+    render_mode = None  # Set to None to run without graphics
 
-env_manager = EnvironmentManager(render_mode=render_mode, environment=environment)
-model = Model(action_space_n=env_manager.env.action_space.n, _discount_factor=discount_factor, _observation_space=env_manager.env.observation_space)
-agent = Agent(model)
+    env_manager = EnvironmentManager(
+        render_mode=render_mode, environment=environment, seed=seed
+    )
+    model = Model(
+        action_space_n=env_manager.env.action_space.n,
+        _discount_factor=discount_factor,
+        _observation_space=env_manager.env.observation_space,
+    )
+    agent = Agent(model)
 
-rewards = 0.
-actions = []
-states = []
-state, info = env_manager.reset()
-states.append(state)
-
-episodes = 0
-finished_training = False
-start = time.time()
-while True:
-    if render_mode == "human":
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key == pygame.K_q):
-                env_manager.close()
-                exit()
-
-    states_mean, states_std = agent.normalize_states()
-    action_rewards, action_weights = agent.compute_action_rewards(state, states_mean, states_std)
-    action = agent.get_action(action_rewards, action_weights)
-
-    actions.append(action)
-    state, reward, terminated, truncated, info = env_manager.step(action)
+    rewards = 0.0
+    actions = []
+    states = []
+    state, info = env_manager.reset()
     states.append(state)
-    rewards += float(reward)
 
-    if terminated or truncated:
-        print(f"rewards: {rewards}")
+    episodes = 0
+    finished_training = False
+    start = time.time()
+    while True:
+        if render_mode == "human":
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN and (event.key == pygame.K_ESCAPE or event.key == pygame.K_q):
+                    env_manager.close()
+                    exit()
 
-        episode_rewards.append(rewards)
+        states_mean, states_std = agent.normalize_states()
+        action_rewards, action_weights = agent.compute_action_rewards(state, states_mean, states_std)
+        action = agent.get_action(action_rewards, action_weights)
 
-        if episodes == training_time and not finished_training:
-            episodes = 0
-            end = time.time()
-            print("Time :{}".format(end-start))
-            env_manager = EnvironmentManager(render_mode="human")
-            
-            model.run_k_means(k=k)
-            model.update_transitions_and_rewards_for_clusters()
-
-            agent.use_clusters = True
-            plot_rewards(episode_rewards=episode_rewards)
-            training_rewards = episode_rewards
-            episode_rewards = []
-            episodes = -1
-            finished_training = True
-
-        elif episodes < training_time and not finished_training:
-            model.update_model(states, actions, rewards)
-
-        if episodes == testing_time and finished_training:
-            testing_rewards = episode_rewards
-
-            
-            data = {
-                "environment" : environment,
-                "discount_factor" : discount_factor,
-                "k" : k,
-                "training_time" : training_time,
-                "testing_time" : testing_time,
-                "training_rewards" : training_rewards,
-                "testing_rewards" : testing_rewards
-            }
-            write_to_json(data)
-
-
-            plot_rewards(episode_rewards=episode_rewards)
-            env_manager.close()
-            exit()
-        
-        rewards = 0.
-        actions.clear()
-        states.clear()
-        state, info = env_manager.reset()
+        actions.append(action)
+        state, reward, terminated, truncated, info = env_manager.step(action)
         states.append(state)
-        episodes += 1
+        rewards += float(reward)
+
+        if terminated or truncated:
+            print(f"rewards: {rewards}")
+
+            episode_rewards.append(rewards)
+
+            if episodes == training_time and not finished_training:
+                episodes = 0
+                end = time.time()
+                print("Time :{}".format(end - start))
+
+                model.run_k_means(k=k)
+                model.update_transitions_and_rewards_for_clusters(gaussian_width=gaussian_width_rewards)
+
+                agent.use_clusters = True
+                # plot_rewards(episode_rewards=episode_rewards)
+                training_rewards = episode_rewards
+                episode_rewards = []
+                episodes = -1
+                finished_training = True
+
+            elif episodes < training_time and not finished_training:
+                model.update_model(states, actions, rewards)
+
+            if episodes == testing_time and finished_training:
+                testing_rewards = episode_rewards
+
+                data = {
+                    "environment": environment,
+                    "discount_factor": discount_factor,
+                    "k": k,
+                    "gaussian_width_rewards": gaussian_width_rewards,
+                    "seed": seed,
+                    "comments": comments,
+                    "training_time": training_time,
+                    "testing_time": testing_time,
+                    "training_rewards": training_rewards,
+                    "testing_rewards": testing_rewards
+                }
+                write_to_json(data)
+
+                # plot_rewards(episode_rewards=episode_rewards)
+                env_manager.close()
+                break
+
+            rewards = 0.
+            actions.clear()
+            states.clear()
+            state, info = env_manager.reset()
+            states.append(state)
+            episodes += 1
